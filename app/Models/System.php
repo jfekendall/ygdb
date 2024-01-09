fc939b3706b38425fc8128f85c6322399d163c25
Justin <jfekendall@gmail.com>
2024-01-09 15:43:51 -0500

    refactor: added a System model that falls more in line with the conventions of the other base models

--- app/Models/System.php
+++ app/Models/System.php
@@ -7,11 +7,37 @@
 
 namespace App\Models;
 
+use CodeIgniter\Model;
+
 /**
  * Description of System
  *
  * @author justin
  */
-class System {
-    //put your code here
+class System extends Model{
+
+    protected $table = 'ygdb_systems';
+    protected $allowedFields = [
+        'system_name',
+        'system_banner',
+        'system_debut',
+        'system_end'
+    ];
+
+    public function getSystem($sys = false) {
+        if ($sys === false) {
+            return false;
+        }
+
+        return $this->where(['id' => $sys])->first();
+    }
+
+    public function getSystemByName($sys = false) {
+        if ($sys === false) {
+            return false;
+        }
+
+        return $this->where(['system_name' => $sys])->first();
+    }
+
 }
