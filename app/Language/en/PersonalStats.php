b79de882d6cc2b6dd7452320ad5ac4f490094f1d
Justin <jfekendall@gmail.com>
2024-01-13 11:55:56 -0500

    feat: translating static text to language file
    This is in an effort to not only encourage more use through
    locallization, but also to make the text easier to manage by being all
    in one spot. This is only the start of a broader effort. Many of the
    normalized fields in the database still need to be language filed (e.g.
    Markets)

---
+++ app/Language/en/PersonalStats.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'personal_stats' => 'Your Stats',
+    'status' => 'Status',
+    'with_case' => 'Case/Box',
+    'in_wrap' => 'In Wrap',
+    'with_manual' => 'Manual'
+];
+
